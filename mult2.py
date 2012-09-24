ef62ac8b66a4058d9732b893be8f16d2a448037c
arjunpassi <lakhan211@yahoo.com>
2012-09-24 01:06:38 -0300

    Update mult2.py

--- mult2.py
+++ mult2.py
@@ -5,14 +5,6 @@ import sys
 import fileinput
 
 
-#parser = argparse.ArgumentParser(description='Process some numbers')
-#parser = argparse.ArgumentParser()
-
-#parser.add_argument('input', nargs = '*', type=argparse.FileType('r'))
-#parser.add_argument('--ignore-blank', type=str,default=0)
-
-#args = parser.parse_args()
-
 ans=1
 inputFlag = 1
 inputNum = 0
@@ -27,11 +19,9 @@ fileArray = []
 while count < len(sys.argv):
     if sys.argv[count] == "--ignore-blank":
         blankFlag = 1
-        print blankFlag
     
     elif sys.argv[count] == "--ignore-non-numeric":
         nonNumericFlag = 1
-        print nonNumericFlag
     
     else:
         fileArray.append(sys.argv[count])
@@ -41,31 +31,33 @@ while count < len(sys.argv):
 
 fileArray.pop(0)   
 
-print fileArray
-
 if len(sys.argv) > 1:
    
    for line in fileinput.input(fileArray):
        try:
-          print line
-          inputNum = float(line)       
           str = line
-          
-          if blankFlag == 1:
-                if str.strip() == "":
-                    print "h"    
-                
+          inputNum = float(line)                     
           ans = ans * inputNum          
 
        except ValueError as e:
        
+            if blankFlag == 1:
+                if str.strip() == "":
+                    continue
             
+            else:
+                if str.strip() == "":
+                    print ans
+                    ans = 1
+                    continue
             
-                
-	        print e	
-	        sys.exit(1)                                        
+            if nonNumericFlag == 1:
+                    continue
+                 
+            print e
+            sys.exit(1)                                             
 	    	    
-   print "\nAnswer :", ans
+   print ans
 	
 
 else:
@@ -82,8 +74,6 @@ else:
 	    print e	
 	    sys.exit(1)
 
-    except EOFError:
-	    print "\nEnd of the File: Pressed CtrlD"
-	    print "\nThe answer is  :", ans
-
+    except EOFError:	    
+	    print ans
 
